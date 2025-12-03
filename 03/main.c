a2a21db341ae0245cdda7c51522fb5958a380e9d
Olga Pavlova <olgapavlova@gmail.com>
2025-12-03 19:14:31 +0300

    03/1

--- 03/main.c
+++ 03/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define FILE_PATH "input.txt"
 
 int main(void) {
 
   FILE * f = fopen(FILE_PATH, "r");
-  if(!f) { abort(); }
-  
 
   char * line;
   size_t len;
@@ -17,16 +16,39 @@ int main(void) {
   int shift;
   char sign;
 
+
+  unsigned long long sum = 0;
+  char sj[3] = {'\0'};
+  int jo = 0;
+
   while((read = getline(&line, &len, f)) != -1) {
-    sscanf(line, "%c%d", &sign, &shift);
+    size_t l = strlen(line);
+    if(line[l - 1] == '\n') {
+      line[l - 1] = '\0'; l--;
+    }
 
-    position += (sign == 'L' ? 1 : -1) * shift;
+    char * p = line;
+    char * q = line;
+    while(*(q+1) != '\0') {
+      if(*p < *q) { p = q; }
+      q++;
+    }
 
-    position = (position + 100) % 100;
+    char * r = p + 1;
+    q = r;
+    while(*q != '\0') {
+      if (*r < *q) { r = q; }
+      q++;
+    }
 
-    printf("%c%d\t%d\t%c\t%s", (sign == 'L' ? '+' : '-'), shift, position, (position == 0 ? '!' : ' '), line);
-   }
+    sprintf(sj, "%c%c", *p, *r);
+    sscanf(sj, "%d", &jo);
+    sum += jo;
 
+    printf("%c%c\t%s\n", *p, *r, line);
+  }
+
+  printf("%lld\n", sum);
 
   fclose(f);
   return 0;
